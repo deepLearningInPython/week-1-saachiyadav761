b111bd18a8ef0f43f57ed81dc0e90b1e6b706d6c
saachiyadav761 <saachiyadav761@gmail.com>
2025-11-03 14:50:22 +0100

    v5

--- tasks.py
+++ tasks.py
@@ -40,7 +40,7 @@ def step(x):
 # Your code here:
 # -----------------------------------------------
 #my code
-def ReLu1(arr, cutoff=0):
+def ReLu(arr, cutoff=0):
     res = []
     for num in arr:
         if num > 0:
@@ -83,7 +83,7 @@ def ReLu1(arr, cutoff=0):
 
 def neural_net_layer(A,B):
     result = A @ B #matrix multiplication
-    return ReLu1(result) #apply ReLu
+    return ReLu(result) #apply ReLu
 
 #lets try
 #print(neural_net_layer(A,B))
